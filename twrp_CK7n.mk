5fc862d780722a8f74a64d0e2f1a723206fbac01
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-10 04:33:32 +0300

    Update twrp_CK7n.mk

--- twrp_CK7n.mk
+++ twrp_CK7n.mk
@@ -5,14 +5,14 @@
 #
 
 # Inherit from TECNO-CK7n device
-$(call inherit-product, device/tecno/TECNO-CK7n/device.mk)
+$(call inherit-product, device/tecno/CK7n/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
 PRODUCT_NAME := twrp_CK7n
-PRODUCT_DEVICE := TECNO-CK7n
+PRODUCT_DEVICE := CK7n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO CK7n
 PRODUCT_MANUFACTURER := TECNO
