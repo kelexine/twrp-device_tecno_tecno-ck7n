bcc65fca4620dd1e08dae9757398981bde2fce04
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-10 04:32:45 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-CK7n
+DEVICE_PATH := device/tecno/CK7n
 
 # Inherit from mt6789-common
 include device/transsion/mt6789-common/BoardConfigCommon.mk
@@ -17,4 +17,4 @@ TARGET_INIT_VENDOR_LIB := libinit_TECNO-CK7n
 TARGET_RECOVERY_DEVICE_MODULES := libinit_TECNO-CK7n
 
 # TWRP Configs
-TW_DEVICE_VERSION := CK7n_by_rama982
+TW_DEVICE_VERSION := rama982-Turkishpatriot
